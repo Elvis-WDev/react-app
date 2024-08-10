6d3ea81329e4808402c33b84dcdebdbbe84e66c7
Elvis <maicol-elvis@hotmail.com>
2024-08-10 12:41:22 -0500

    Sudia cambios

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -1,5 +1,7 @@
 import { Navigate, NavLink, Route, Routes } from "react-router-dom";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
+import LazyPage1 from "../pages/LazyPage1";
+import LazyPage2 from "../pages/LazyPage2";
+import LazyPage3 from "../pages/LazyPage3";
 
 const LazyLayout = () => {
     return (
@@ -21,9 +23,9 @@ const LazyLayout = () => {
 
             <Routes>
 
-                <Route path="lazy1" element={LazyPage1} />
-                <Route path="lazy2" element={LazyPage2} />
-                <Route path="lazy3" element={LazyPage3} />
+                <Route path="lazy1" element={<LazyPage1 />} />
+                <Route path="lazy2" element={<LazyPage2 />} />
+                <Route path="lazy3" element={<LazyPage3 />} />
 
                 <Route path="*" element={<Navigate replace to="lazy1" />} />
 
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -4,7 +4,7 @@
 const LazyPage3 = () => {
     return (
 
-        <h1>LAZY PAGE 1</h1>
+        <h1>LAZY PAGE 3</h1>
 
     )
 }
